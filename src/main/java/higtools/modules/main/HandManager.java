44072f8a425bb88a7fde433cc3b9617bb45bbce5
RedCarlos <58893097+RedCarlos26@users.noreply.github.com>
2022-06-10 21:28:34 -0400

    Oopsies

--- src/main/java/higtools/modules/main/HandManager.java
+++ src/main/java/higtools/modules/main/HandManager.java
@@ -7,16 +7,27 @@
 package higtools.modules.main;
 
 import higtools.modules.HIGTools;
+import meteordevelopment.meteorclient.events.meteor.MouseButtonEvent;
 import meteordevelopment.meteorclient.events.world.TickEvent;
 import meteordevelopment.meteorclient.settings.BoolSetting;
 import meteordevelopment.meteorclient.settings.EnumSetting;
 import meteordevelopment.meteorclient.settings.Setting;
 import meteordevelopment.meteorclient.settings.SettingGroup;
+import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.meteorclient.systems.modules.Modules;
+import meteordevelopment.meteorclient.systems.modules.combat.AutoTotem;
+import meteordevelopment.meteorclient.systems.modules.combat.CrystalAura;
+import meteordevelopment.meteorclient.utils.misc.input.KeyAction;
 import meteordevelopment.meteorclient.utils.player.FindItemResult;
 import meteordevelopment.meteorclient.utils.player.InvUtils;
 import meteordevelopment.orbit.EventHandler;
+import net.minecraft.item.AxeItem;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.item.SwordItem;
+
+import static org.lwjgl.glfw.GLFW.GLFW_MOUSE_BUTTON_RIGHT;
 
 public class HandManager extends Module {
     private final SettingGroup sgGeneral = settings.getDefaultGroup();
@@ -36,6 +47,14 @@ public class HandManager extends Module {
         .build()
     );
 
+    private final Setting<Boolean> rightClick = sgGeneral.add(new BoolSetting.Builder()
+        .name("right-click")
+        .description("Only holds the item in your offhand when you are holding right click.")
+        .defaultValue(false)
+        .build()
+    );
+
+    private boolean isClicking;
     private boolean sentMessage;
     private Item currentItem;
 
@@ -46,11 +65,15 @@ public class HandManager extends Module {
     @Override
     public void onActivate() {
         sentMessage = false;
+        isClicking = false;
         currentItem = item.get();
     }
 
     @EventHandler
     private void onTick(TickEvent.Pre event) {
+        AutoTotem autoTotem = Modules.get().get(AutoTotem.class);
+
+        currentItem = item.get();
 
         // Checking offhand item
         if (mc.player.getOffHandStack().getItem() != currentItem.item) {
@@ -63,7 +86,39 @@ public class HandManager extends Module {
                     sentMessage = true;
                 }
             }
+
+            // Swap to offhand
+            else if ((isClicking || !rightClick.get()) && !autoTotem.isLocked() && !item.isOffhand()) {
+                InvUtils.move().from(item.slot()).toOffhand();
+                sentMessage = false;
+            }
         }
+
+        // If not clicking, set to totem if auto totem is on
+        else if (!isClicking && rightClick.get()) {
+            if (autoTotem.isActive()) {
+                FindItemResult totem = InvUtils.find(itemStack -> itemStack.getItem() == Items.TOTEM_OF_UNDYING, hotbar.get() ? 0 : 9, 35);
+
+                if (totem.found() && !totem.isOffhand()) {
+                    InvUtils.move().from(totem.slot()).toOffhand();
+                }
+            } else {
+                FindItemResult empty = InvUtils.find(ItemStack::isEmpty, hotbar.get() ? 0 : 9, 35);
+                if (empty.found()) InvUtils.move().fromOffhand().to(empty.slot());
+            }
+        }
+    }
+
+    @EventHandler
+    private void onMouseButton(MouseButtonEvent event) {
+        isClicking = mc.currentScreen == null && !Modules.get().get(AutoTotem.class).isLocked() && !usableItem() && !mc.player.isUsingItem() && event.action == KeyAction.Press && event.button == GLFW_MOUSE_BUTTON_RIGHT;
+    }
+
+    private boolean usableItem() {
+        return mc.player.getMainHandStack().getItem() == Items.BOW
+            || mc.player.getMainHandStack().getItem() == Items.TRIDENT
+            || mc.player.getMainHandStack().getItem() == Items.CROSSBOW
+            || mc.player.getMainHandStack().getItem().isFood();
     }
 
     @Override
