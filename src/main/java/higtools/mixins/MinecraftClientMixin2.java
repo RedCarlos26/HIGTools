9017e89561b650218697de88511aa6fb5276b30c
RedCarlos <58893097+RedCarlos26@users.noreply.github.com>
2022-06-15 21:11:12 -0400

    Added custom window icon

--- src/main/java/higtools/mixins/MinecraftClientMixin2.java
+++ src/main/java/higtools/mixins/MinecraftClientMixin2.java
@@ -7,16 +7,29 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import net.minecraft.client.util.Window;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 @Mixin(
     value = {MinecraftClient.class},
     priority = 1001
 )
-public abstract class MincraftClientMixin implements IMinecraftClient {
+public abstract class MinecraftClientMixin2 implements IMinecraftClient {
 
     @Inject(method = "getWindowTitle", at = @At("HEAD"), cancellable = true)
     public void getWindowTitle(CallbackInfoReturnable<String> ci){
         String title = "HIG Tools " + HIGTools.VERSION;
         ci.setReturnValue(title);
+    }
+
+    @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/Window;setIcon(Ljava/io/InputStream;Ljava/io/InputStream;)V"))
+    public void setAlternativeWindowIcon(Window window, InputStream inputStream1, InputStream inputStream2) throws IOException {
+        window.setIcon(
+            HIGTools.class.getResourceAsStream("/assets/higtools/16x.png"),
+            HIGTools.class.getResourceAsStream("/assets/higtools/32x.png")
+        );
     }
 }
--- src/main/java/higtools/modules/HIGTools.java
+++ src/main/java/higtools/modules/HIGTools.java
@@ -18,6 +18,7 @@ import net.minecraft.item.Items;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.lang.invoke.MethodHandles;
 
 import static higtools.AdapterKt.*;
