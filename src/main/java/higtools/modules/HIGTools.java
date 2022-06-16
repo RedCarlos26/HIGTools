895cb42ba5815739822567ec042caa348c975254
RedCarlos <58893097+RedCarlos26@users.noreply.github.com>
2022-06-15 20:46:34 -0400

    I got version trolled

--- src/main/java/higtools/modules/HIGTools.java
+++ src/main/java/higtools/modules/HIGTools.java
@@ -24,7 +24,7 @@ import static higtools.AdapterKt.*;
 
 public class HIGTools extends MeteorAddon {
 	public static final Logger LOG = LoggerFactory.getLogger("HIG Tools");
-    public static final String VERSION = "1.1.1";
+    public static final String VERSION = "1.2";
     public static final Category HIG = new Category("HIG Tools", Items.NETHERITE_PICKAXE.getDefaultStack());
 
 	@Override
