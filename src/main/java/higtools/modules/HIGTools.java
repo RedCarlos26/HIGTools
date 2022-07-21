dddb002507d3579a5cf4f19bc12f37e0bb113026
RedCarlos26 <58893097+RedCarlos26@users.noreply.github.com>
2022-07-21 11:12:37 -0400

    Future version 2.0

--- src/main/java/higtools/modules/HIGTools.java
+++ src/main/java/higtools/modules/HIGTools.java
@@ -24,7 +24,7 @@ import java.lang.invoke.MethodHandles;
 import static higtools.AdapterKt.*;
 
 public class HIGTools extends MeteorAddon {
-    public static String VERSION = "1.9.1";
+    public static String VERSION = "2.0-dev";
 	public static final Logger LOG = LoggerFactory.getLogger("HIG Tools");
     public static final Category MAIN = new Category("HIG Tools", Items.NETHERITE_PICKAXE.getDefaultStack());
     public static final Category BORERS = new Category(" Borers ", Items.NETHERITE_PICKAXE.getDefaultStack());
