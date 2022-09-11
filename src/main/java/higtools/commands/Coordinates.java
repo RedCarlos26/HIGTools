1eba0f1686d9d6e6aca84013e214071ede00dc71
RedCarlos26 <58893097+RedCarlos26@users.noreply.github.com>
2022-09-10 22:32:44 -0400

    More cleaning

--- src/main/java/higtools/commands/Coordinates.java
+++ src/main/java/higtools/commands/Coordinates.java
@@ -16,7 +16,7 @@ public class Coordinates extends Command {
     public void build(LiteralArgumentBuilder<CommandSource> builder) {
         builder.executes(context -> {
             mc.keyboard.setClipboard(mc.player.getBlockPos().getX() + ", " + mc.player.getBlockPos().getY() + ", " + mc.player.getBlockPos().getZ());
-            mc.getToastManager().add(new SystemToast(SystemToast.Type.TUTORIAL_HINT, Text.of("Coordinates copied to clipboard."), Text.of("Coordinates copied to clipboard.")));
+            mc.getToastManager().add(new SystemToast(SystemToast.Type.TUTORIAL_HINT, Text.of("HIGTools"), Text.of("Coordinates copied to clipboard.")));
             return SINGLE_SUCCESS;
         });
     }
--- src/main/java/higtools/mixins/MinecraftClientMixin2.java
+++ src/main/java/higtools/mixins/MinecraftClientMixin2.java
@@ -7,11 +7,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import net.minecraft.client.util.Window;
-import org.spongepowered.asm.mixin.injection.Redirect;
-
-import java.io.IOException;
-import java.io.InputStream;
 
 @Mixin(
     value = {MinecraftClient.class},
