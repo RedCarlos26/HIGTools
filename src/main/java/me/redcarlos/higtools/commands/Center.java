054f18bb801668f27dbcb77ae609490195161cec
RickyTheRacc <rickytheracc@gmail.com>
2024-07-18 20:07:19 -0500

    Use PlayerUtils in Center Command
    This does the exact same thing, no need to try and make it look complex

--- src/main/java/me/redcarlos/higtools/commands/Center.java
+++ src/main/java/me/redcarlos/higtools/commands/Center.java
@@ -2,6 +2,8 @@ package me.redcarlos.higtools.commands;
 
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import meteordevelopment.meteorclient.commands.Command;
+import meteordevelopment.meteorclient.utils.Utils;
+import meteordevelopment.meteorclient.utils.player.PlayerUtils;
 import net.minecraft.command.CommandSource;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 import net.minecraft.util.math.MathHelper;
@@ -14,11 +16,7 @@ public class Center extends Command {
     @Override
     public void build(LiteralArgumentBuilder<CommandSource> builder) {
         builder.executes(context -> {
-            double x = MathHelper.floor(mc.player.getX()) + 0.5;
-            double z = MathHelper.floor(mc.player.getZ()) + 0.5;
-            mc.player.setPosition(x, mc.player.getY(), z);
-            mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.PositionAndOnGround(mc.player.getX(), mc.player.getY(), mc.player.getZ(), mc.player.isOnGround()));
-
+            PlayerUtils.centerPlayer();
             return SINGLE_SUCCESS;
         });
     }
