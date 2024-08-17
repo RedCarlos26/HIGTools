f0f2114539d81770b24e1f9cb1f89baa0443e8c1
RickyTheRacc <rickytheracc@gmail.com>
2024-08-17 09:48:14 -0500

    Add setting

--- src/main/java/me/redcarlos/higtools/mixins/RotationMixin.java
+++ src/main/java/me/redcarlos/higtools/mixins/RotationMixin.java
@@ -2,6 +2,9 @@ package me.redcarlos.higtools.mixins;
 
 import meteordevelopment.meteorclient.events.game.GameLeftEvent;
 import meteordevelopment.meteorclient.events.game.OpenScreenEvent;
+import meteordevelopment.meteorclient.settings.BoolSetting;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.modules.Categories;
 import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.meteorclient.systems.modules.player.Rotation;
@@ -9,9 +12,26 @@ import meteordevelopment.orbit.EventHandler;
 import net.minecraft.client.gui.screen.DisconnectedScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = Rotation.class, remap = false)
 public abstract class RotationMixin extends Module {
+    @Unique private Setting<Boolean> toggleOnLeave;
+
+    @Inject(method = "<init>", at=@At("TAIL"))
+    private void onInit(CallbackInfo ci) {
+        SettingGroup sgGeneral = settings.getDefaultGroup();
+
+        toggleOnLeave  = sgGeneral.add(new BoolSetting.Builder()
+            .name("toggle-on-leave")
+            .description("Toggle the module off when you leave a world.")
+            .defaultValue(false)
+            .build()
+        );
+    }
+
     public RotationMixin() {
         super(Categories.Player, "rotation", "Changes/locks your yaw and pitch.");
     }
@@ -19,13 +39,17 @@ public abstract class RotationMixin extends Module {
     @Unique
     @EventHandler
     private void onGameLeft(GameLeftEvent event) {
+        if (!toggleOnLeave.get() || !isActive()) return;
+
         toggle();
     }
 
     @Unique
     @EventHandler
     private void onScreenOpen(OpenScreenEvent event) {
-        if (event.screen instanceof DisconnectedScreen) toggle();
-    }
+        if (!toggleOnLeave.get() || !isActive()) return;
+        if (!(event.screen instanceof DisconnectedScreen)) return;
 
+        toggle();
+    }
 }
