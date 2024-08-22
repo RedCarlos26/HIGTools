31e739e2c3bd78405d0c54768ed5562dec0584c7
RickyTheRacc <rickytheracc@gmail.com>
2024-08-22 08:58:49 -0500

    Readd position presets
    And make note of the differences from Meteor so they aren't removed in the future

--- src/main/java/me/redcarlos/higtools/modules/hud/TextPresets.java
+++ src/main/java/me/redcarlos/higtools/modules/hud/TextPresets.java
@@ -12,6 +12,9 @@ public class TextPresets {
     }
 
     static {
+        // These use a different and more accurate rounding method than Meteor's, do not remove
+        addPreset("Player Coords", "XYZ: #1{roundToString(player.pos.x, 1)}, {floor(player.pos.y)}, {roundToString(player.pos.z, 1)}");
+        addPreset("Opposite-Coords", "{player.opposite_dimension != \"End\" ? player.opposite_dimension + \":\" : \"\"} #1{player.opposite_dimension != \"End\" ? \"\" + floor(player.opposite_dim_pos.x) + \", \" + floor(player.opposite_dim_pos.z) : \"\"}");
 
         addPreset("Welcome Hud", "Welcome to HIG Tools, #1{meteor.is_module_active(\"name-protect\") ?  meteor.get_module_setting(\"name-protect\", \"name\") : player._toString}");
         addPreset("KM/H Speed", "Speed: #1{roundToString(player.speed * 3.6, 1)} km/h");
