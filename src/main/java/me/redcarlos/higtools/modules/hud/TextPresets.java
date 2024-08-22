8953136fbcf040fac835f4e8fe01f0d065d12448
RickyTheRacc <rickytheracc@gmail.com>
2024-08-21 20:37:15 -0500

    Cleanup text presets
    Player coords and opposite coords are both implemented in Meteor now, also can inline some of the values in addPreset

--- src/main/java/me/redcarlos/higtools/modules/hud/TextPresets.java
+++ src/main/java/me/redcarlos/higtools/modules/hud/TextPresets.java
@@ -12,15 +12,13 @@ public class TextPresets {
     }
 
     static {
-        addPreset("Player-Coords", "XYZ: #1{roundToString(player.pos.x, 1)}, {floor(player.pos.y)}, {roundToString(player.pos.z, 1)}", 0);
-        addPreset("Opposite-Coords", "{player.opposite_dimension != \"End\" ? player.opposite_dimension + \":\" : \"\"} #1{player.opposite_dimension != \"End\" ? \"\" + floor(player.opposite_dim_pos.x) + \", \" + floor(player.opposite_dim_pos.z) : \"\"}", 0);
-        addPreset("KM/H-Speed", "Speed: #1{roundToString(player.speed*3.6, 1)} km/h", 0);
+        addPreset("KM/H Speed", "Speed: #1{roundToString(player.speed*3.6, 1)} km/h");
     }
 
-    private static HudElementInfo<TextHud>.Preset addPreset(String title, String text, int updateDelay) {
-        return INFO.addPreset(title, textHud -> {
+    private static void addPreset(String title, String text) {
+        INFO.addPreset(title, textHud -> {
             if (text != null) textHud.text.set(text);
-            if (updateDelay != -1) textHud.updateDelay.set(updateDelay);
+            textHud.updateDelay.set(0);
         });
     }
 }
